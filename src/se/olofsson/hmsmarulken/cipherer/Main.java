aa941769fb1cfc81db06c892b6b11941602fc539
Chris_Acrobat <chrisacrobat88@gmail.com>
2016-06-11 12:53:48 +0200

    Changed look and feel to SystemLookAndFeel.

--- src/se/olofsson/hmsmarulken/cipherer/Main.java
+++ src/se/olofsson/hmsmarulken/cipherer/Main.java
@@ -15,7 +15,27 @@ public class Main
 
     public static void main(String[] args)
     {
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(UnsupportedLookAndFeelException e)
+        {
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch(InstantiationException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+
         new LiveCipher().setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        //new Cipherer().getNewCipher();
     }
 }
