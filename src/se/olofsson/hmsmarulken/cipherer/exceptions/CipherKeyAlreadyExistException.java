44831e30f7f0474c754b7bc8708161c83b15458b
Chris_Acrobat <chrisacrobat88@gmail.com>
2016-06-11 12:46:15 +0200

    Renamed one exception.

--- src/se/olofsson/hmsmarulken/cipherer/exceptions/CipherKeyAlreadyExistException.java
+++ src/se/olofsson/hmsmarulken/cipherer/exceptions/CipherKeyAlreadyExistException.java
@@ -4,9 +4,9 @@ package se.olofsson.hmsmarulken.cipherer.exceptions;
  * Created on 2016-05-29.
  * @author Christoffer Olofsson
  */
-public class CipherAlreadyExistException extends Exception
+public class CipherKeyAlreadyExistException extends Exception
 {
-    public CipherAlreadyExistException(int leftRoller, int middleRoller, int rightRoller)
+    public CipherKeyAlreadyExistException(int leftRoller, int middleRoller, int rightRoller)
     {
         super("Cipherer (" + leftRoller + ", " + middleRoller + ", " + rightRoller + ") already exist.");
     }
